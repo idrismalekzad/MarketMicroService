5999e208f71cef4d094e0efd532a23b114b65a4b
soheilpmr <sohail.pmreza@gmail.com>
2022-05-27 21:36:48 +0430

    addcontrollers

---
+++ MarketMicroService/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using MarketMicroService.DtoObjects;
+using MarketMicroService.Services.Intefaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace MarketMicroService.Controllers
+{
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = categoryService.GetCategories();
+            return Ok(data);
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public IActionResult Post([FromBody] CategoryDto categoryDto)
+        {
+            categoryService.AddNewCatrgory(categoryDto);
+            return Ok();
+        }
+
+    }
+}
---
+++ MarketMicroService/Controllers/ProductAdminController.cs
@@ -0,0 +1,28 @@
+﻿using MarketMicroService.DtoObjects;
+using MarketMicroService.Services.Intefaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketMicroService.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class ProductAdminController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductAdminController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] AddNewProductDto addNewProductDto)
+        {
+            productService.AddNewProduct(addNewProductDto);
+            return Ok();
+        }
+
+    }
+}
+
