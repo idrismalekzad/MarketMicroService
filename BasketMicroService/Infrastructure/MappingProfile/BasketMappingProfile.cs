6e2dacccf28285fbd64e7008c06027ad947bbd8a
soheilpmr <sohail.pmreza@gmail.com>
2022-06-05 21:18:34 +0430

    add basket service

---
+++ BasketMicroService/Infrastructure/MappingProfile/BasketMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BasketMicroService.DtoObjects;
+using BasketService.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketService.Infrastructure.MappingProfile
+{
+    public class BasketMappingProfile : Profile
+    {
+        public BasketMappingProfile()
+        {
+            CreateMap<BasketItem, AddItemToBasketDto>().ReverseMap();
+        }
+    }
+}
---
+++ BasketMicroService/Service/Interfaces/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using BasketMicroService.DtoObjects;
+using System;
+
+namespace BasketMicroService.Service.Interfaces
+{
+    public interface IBasketService
+    {
+        BasketDto GetOrCreateBasketForUser(string UserId);
+        BasketDto GetBasket(string UserId);
+        void AddItemToBasket(AddItemToBasketDto item);
+        void RemoveItemFromBasket(Guid ItemId);
+        void SetQuantities(Guid itemId, int quantity);
+        void TransferBasket(string anonymousId, string UserId);
+
+    }
+}
