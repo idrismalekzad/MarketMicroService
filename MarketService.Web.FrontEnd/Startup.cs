d89e826eaf1fdee0bfc17b6b56cc42b94def24b0
soheil.pmr <sohail.pmreza@gmail.com>
2023-02-18 18:34:37 +0330

    Update Startup.cs

--- MarketService.Web.FrontEnd/Startup.cs
+++ MarketService.Web.FrontEnd/Startup.cs
@@ -1,9 +1,11 @@
+using MarketService.Web.FrontEnd.Services.ProductServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,13 @@ namespace MarketService.Web.FrontEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<IProductService>(p =>
+            {
+                return new ProductService(
+                    new RestClient(Configuration["MicroservicAddress:Product:uri"]));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
