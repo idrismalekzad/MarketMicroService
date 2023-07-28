9ddbba088dda69a985cb084a24c844b50021278b
devops_faciliman <sohail.pmreza@gmail.com>
2023-07-28 17:17:54 +0330

    update ordermicroservice to .Net 7

--- ORderMIcrsservice/Program.cs
+++ ORderMIcrsservice/Program.cs
@@ -1,26 +1,53 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OrderMicrsService.Infrasttructure.Context;
+using OrderMicrsService.Service.Implemetions;
+using OrderMicrsService.Service.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ORderMIcrsservice
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var builder = WebApplication.CreateBuilder(args);
+
+var Configuration = builder.Configuration;
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<OrderDataBaseContext>(o => o.UseSqlServer
+    (Configuration["OrderConnection"]));
+
+
+builder.Services.AddTransient<IOrderService, OrderService>();
+
+
+// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
