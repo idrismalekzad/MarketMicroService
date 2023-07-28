ef0ee0f5c286e7a15584f779219e1e862c9a0f52
devops_faciliman <sohail.pmreza@gmail.com>
2023-07-28 17:09:32 +0330

    Update DiscountService to .Net7

--- DiscountMicroService/Program.cs
+++ DiscountMicroService/Program.cs
@@ -1,26 +1,43 @@
+using DiscountMicroService.Infrastructure.Context;
+using DiscountMicroService.Infrastructure.MappingProfile;
+using DiscountMicroService.Service.Implementation;
+using DiscountMicroService.Service.Interfaces;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace DiscountMicroService
+var builder = WebApplication.CreateBuilder(args);
+
+var Configuration = builder.Configuration;
+
+// This method gets called by the runtime. Use this method to add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddTransient<IDsicountService, DiscountService>();
+
+builder.Services.AddDbContext<DiscountDataBaseDontext>(o => o.UseSqlServer(Configuration["DiscountConnection"]));
+
+builder.Services.AddAutoMapper(typeof(DiscountMappingProfile));
+
+
+// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
