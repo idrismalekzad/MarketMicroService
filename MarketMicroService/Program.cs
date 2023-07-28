b919af811c6270954375bc890d925ed955a63fff
devops_faciliman <sohail.pmreza@gmail.com>
2023-07-28 17:21:54 +0330

    uPdate ProductMicroservice to .Net 7

--- MarketMicroService/Program.cs
+++ MarketMicroService/Program.cs
@@ -1,26 +1,53 @@
+using MarketMicroService.Infrastructure;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MarketMicroService
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var Configuration = builder.Configuration;
+
+// This method gets called by the runtime. Use this method to add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MarketMicroService", Version = "v1" });
+});
+
+builder.Services.AddDbContext<ProductDatabaseContext>(p =>
+                         p.UseSqlServer(Configuration["ProductConnection"]));
+
+
+// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MarketMicroService v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
