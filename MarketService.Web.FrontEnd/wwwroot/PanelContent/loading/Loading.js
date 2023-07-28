e4379e6232726ff0dab81bfdb05ce7c4dbe1e320
devops_faciliman <sohail.pmreza@gmail.com>
2023-07-28 18:24:56 +0330

    Add Ui Infrastructure

---
+++ MarketService.Web.FrontEnd/wwwroot/PanelContent/loading/Loading.js
@@ -0,0 +1,100 @@
+﻿
+//idElement:string
+//status:true/false
+//isBtn:true/false
+function loading(idElement, status, isBtn = false) {
+    var loadigBox = `<div id="myLoading" class="card-overlay-loading"><span class="mySpinner"></span></div>`;
+    var loadingBoxBtn = `<span id="myLoadingBtn" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+    
+    if (isBtn) {
+        if (status === true) {
+            if (document.querySelector(`#${idElement} #myLoadingBtn`) == undefined) {
+                $(`#${idElement}`).prepend(loadingBoxBtn);
+                $(`#${idElement}`).prop('disabled', true);
+            }
+        }
+        else if (status === false) {
+            $(`#${idElement}`).prop('disabled', false);
+            $(`#${idElement} #myLoadingBtn`).remove();
+        }
+    }  
+    else {
+        if (status === true) {
+            document.getElementById(idElement).style.overflow = "hidden";
+            document.getElementById(idElement).style.position = "relative";
+            $(`#${idElement}`).prop('disabled', true);
+
+            if (document.querySelector(`#${idElement} #myLoading`) == undefined) {
+                $(`#${idElement}`).prepend(loadigBox);
+            }
+        }
+        else if (status === false) {
+            document.getElementById(idElement).style.overflow = "unset";
+            document.getElementById(idElement).style.position = "unset";
+            $(`#${idElement}`).prop('disabled', false);
+
+            $(`#${idElement} #myLoading`).remove();
+        }
+    }
+
+
+}
+
+function loadingClass(ClassElement, status, isBtn = false) {
+    var loadigBox = `<div id="myLoading" class="card-overlay-loading"><span class="mySpinner"></span> لطفا منتظر بمانید</div>`;
+    var loadingBoxBtn = `<span id="myLoadingBtn" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+
+    if (isBtn) {
+        if (status === true) {
+            if (document.querySelector(`.${ClassElement} #myLoadingBtn`) == undefined) {
+                $(`.${ClassElement}`).prepend(loadingBoxBtn);
+                $(`.${ClassElement}`).prop('disabled', true);
+            }
+        }
+        else if (status === false) {
+            $(`.${ClassElement}`).prop('disabled', false);
+            $(`.${ClassElement} #myLoadingBtn`).remove();
+        }
+    }
+    else {
+        if (status === true) {
+            $('.' + ClassElement).last().css('overflow','hidden');
+            $('.' + ClassElement).last().css('position','relative');
+            $(`.${ClassElement}`).prop('disabled', true);
+
+            if (document.querySelector(`.${ClassElement} #myLoading`) == undefined) {
+                $(`.${ClassElement}`).prepend(loadigBox);
+            }
+        }
+        else if (status === false) {
+            $('.' + ClassElement).last().css('overflow', 'unset');
+            $('.' + ClassElement).last().css('position', 'unset');
+            
+            $(`.${ClassElement}`).prop('disabled', false);
+
+            $(`.${ClassElement} #myLoading`).remove();
+        }
+    }
+
+}
+
+function loadingPlugin(areaBtn, status) {
+    var loadingBoxBtn = `<span id="myLoadingBtn" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+    
+    var areaId = areaBtn.prevObject[0].id;
+
+    var btnId = areaBtn[0].id;
+
+
+    if (status === true) {
+        if (document.querySelector(`#${areaId} #${btnId} #myLoadingBtn`) == undefined) {
+            $(`#${areaId} #${btnId}`).prepend(loadingBoxBtn);
+            $(`#${areaId} #${btnId}`).prop('disabled', true);
+        }
+    }
+    else if (status === false) {
+        $(`#${areaId} #${btnId}`).prop('disabled', false);
+        $(`#${areaId} #${btnId} #myLoadingBtn`).remove();
+    }
+   
+}
---
+++ MarketService.Web.FrontEnd/wwwroot/PanelContent/rtl/assets/plugins/chartjs/js/chartjs-custom.js
@@ -0,0 +1,403 @@
+$(function () {
+	"use strict";
+	// chart 1
+	//var ctx = document.getElementById('chart1').getContext('2d');
+	//var myChart = new Chart(ctx, {
+	//	type: 'line',
+	//	data: {
+	//		labels: ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنج شنبه', 'جمعه'],
+	//		datasets: [{
+	//			label: 'گوگل',
+	//			data: [16, 20, 14, 12, 17, 18, 10],
+	//			backgroundColor: "transparent",
+	//			borderColor: "#8833ff",
+	//			pointRadius: "0",
+	//			borderWidth: 4
+	//		}, {
+	//			label: 'فیسبوک',
+	//			data: [15, 30, 16, 23, 18, 14, 11],
+	//			backgroundColor: "transparent",
+	//			borderColor: "#29cc39",
+	//			pointRadius: "0",
+	//			borderWidth: 4
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			display: true,
+	//			rtl: true,
+	//			labels: {
+	//				fontColor: '#585757',
+	//				boxWidth: 40
+	//			}
+	//		},
+	//		tooltips: {
+	//			rtl: true,
+	//			enabled: false
+	//		},
+	//		scales: {
+	//			xAxes: [{
+	//				ticks: {
+	//					beginAtZero: true,
+	//					fontColor: '#585757'
+	//				},
+	//				gridLines: {
+	//					display: true,
+	//					color: "rgba(0, 0, 0, 0.07)"
+	//				},
+	//			}],
+	//			yAxes: [{
+	//				ticks: {
+	//					beginAtZero: true,
+	//					fontColor: '#585757'
+	//				},
+	//				gridLines: {
+	//					display: true,
+	//					color: "rgba(0, 0, 0, 0.07)"
+	//				},
+	//			}]
+	//		}
+	//	}
+	//});
+	//// chart 2
+	//var ctx = document.getElementById("chart2").getContext('2d');
+	//var myChart = new Chart(ctx, {
+	//	type: 'bar',
+	//	data: {
+	//		labels: ['401_مرداد', '401_شهریور', 'مهر_401', '401_آبان', 'آذر_401', '401_دی', 'بهمن_401','401_اسفند' ],
+	//		datasets: [{
+	//			label: 'مبلغی که باید پرداخت شود',
+	//			data: [100000000, 250000000, 305000000, 385000000, 495000000, 595000000, 685000000, 900000000],
+	//			barPercentage: .5,
+	//			backgroundColor: "#18add7"
+	//		}, {
+	//			label: 'مبلغ پرداخت شده',
+	//			data: [90000000, 100000000, 250000000, 300000000, 400000000, 440000000, 500000000, 900000000],
+	//			barPercentage: .5,
+	//			backgroundColor: "#29cc39"
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			display: true,
+	//			rtl: true,
+	//			labels: {
+	//				fontColor: '#585757',
+	//				boxWidth: 40
+	//			}
+	//		},
+	//		tooltips: {
+	//			rtl: true,
+	//			enabled: true
+	//		},
+	//		scales: {
+	//			xAxes: [{
+	//				ticks: {
+	//					beginAtZero: true,
+	//					fontColor: '#585757'
+	//				},
+	//				gridLines: {
+	//					display: true,
+	//					color: "rgba(0, 0, 0, 0.07)"
+	//				},
+	//			}],
+	//			yAxes: [{
+	//				ticks: {
+	//					beginAtZero: true,
+	//					fontColor: '#585757'
+	//				},
+	//				gridLines: {
+	//					display: true,
+	//					color: "rgba(0, 0, 0, 0.07)"
+	//				},
+	//			}]
+	//		}
+	//	}
+	//});
+	//// chart 3
+	//new Chart(document.getElementById("chart3"), {
+	//	type: 'pie',
+	//	data: {
+	//		labels: ["آفریقا", "آسیا", "اروپا", "آمریکا لاتین", "آمریکا شمالی"],
+	//		datasets: [{
+	//			label: "جمعیت (میلیون)",
+	//			backgroundColor: ["#8833ff", "#212529", "#17a00e", "#29cc39", "#ffc107"],
+	//			data: [2478, 5267, 734, 784, 433]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'جمعیت پیش بینی شده جهان (میلیون) در 2050'
+	//		}
+	//	}
+	//});
+	//// chart 4
+	//new Chart(document.getElementById("chart4"), {
+	//	type: 'radar',
+	//	data: {
+	//		labels: ["آفریقا", "آسیا", "اروپا", "آمریکا لاتین", "آمریکا شمالی"],
+	//		datasets: [{
+	//			label: "1950",
+	//			fill: true,
+	//			backgroundColor: "rgb(13 110 253 / 23%)",
+	//			borderColor: "#8833ff",
+	//			pointBorderColor: "#fff",
+	//			pointBackgroundColor: "rgba(179,181,198,1)",
+	//			data: [8.77, 55.61, 21.69, 6.62, 6.82]
+	//		}, {
+	//			label: "2050",
+	//			fill: true,
+	//			backgroundColor: "rgba(255,99,132,0.2)",
+	//			borderColor: "rgba(255,99,132,1)",
+	//			pointBorderColor: "#fff",
+	//			pointBackgroundColor: "rgba(255,99,132,1)",
+	//			pointBorderColor: "#fff",
+	//			data: [25.48, 54.16, 7.61, 8.06, 4.45]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'توزیع جمعیت جهان'
+	//		}
+	//	}
+	//});
+	//// chart 5
+	//new Chart(document.getElementById("chart5"), {
+	//	type: 'polarArea',
+	//	data: {
+	//		labels: ["آفریقا", "آسیا", "اروپا", "آمریکا لاتین", "آمریکا شمالی"],
+	//		datasets: [{
+	//			label: "جمعیت (میلیون)",
+	//			backgroundColor: ["#8833ff", "#212529", "#17a00e", "#29cc39", "#ffc107"],
+	//			data: [2478, 5267, 734, 784, 433]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'جمعیت پیش بینی شده جهان (میلیون) در 2050'
+	//		}
+	//	}
+	//});
+	//// chart 6
+	//new Chart(document.getElementById("chart6"), {
+	//	type: 'doughnut',
+	//	data: {
+	//		labels: ["آفریقا", "آسیا", "اروپا", "آمریکا لاتین", "آمریکا شمالی"],
+	//		datasets: [{
+	//			label: "جمعیت (میلیون)",
+	//			backgroundColor: ["#8833ff", "#212529", "#17a00e", "#29cc39", "#ffc107"],
+	//			data: [2478, 5267, 734, 784, 433]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'جمعیت پیش بینی شده جهان (میلیون) در 2050'
+	//		}
+	//	}
+	//});
+	// chart 7
+	new Chart(document.getElementById("chart7"), {
+		type: 'horizontalBar',
+		data: {
+			labels: ["جلسه کمیته اصلی","جلسه کمیته فرعی", "بررسی کمیته تخصصی", "تایید بررسی مدارک", "بررسی تسهیلات", "درخواست تحصیلات"],
+			datasets: [{
+				label: "جمعیت (میلیون)",
+				backgroundColor: ["#8833ff", "#212529", "#17a00e", "#29cc39", "#f71515", "#18add7"],
+				data: [6, 5, 7, 1, 10, 5]
+			}]
+		},
+		options: {
+			maintainAspectRatio: false,
+			legend: {
+				rtl: true,
+				display: false
+			},
+			tooltips: {
+				rtl: true
+			},
+			title: {
+				display: true, 
+				text: 'تعداد روز های سپری شده در هر مرحله از دریافت تحصیلات'
+			}
+			
+		}
+	});
+	//// chart 8
+	//new Chart(document.getElementById("chart8"), {
+	//	type: 'bar',
+	//	data: {
+	//		labels: ["1900", "1950", "1999", "2050"],
+	//		datasets: [{
+	//			label: "آفریقا",
+	//			backgroundColor: "#8833ff",
+	//			data: [133, 221, 783, 2478]
+	//		}, {
+	//			label: "اروپا",
+	//			backgroundColor: "#29cc39",
+	//			data: [408, 547, 675, 734]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'رشد جمعیت (میلیون)'
+	//		}
+	//	}
+	//});
+	//// chart 9
+	//new Chart(document.getElementById("chart9"), {
+	//	type: 'bar',
+	//	data: {
+	//		labels: ["1900", "1950", "1999", "2050"],
+	//		datasets: [{
+	//			label: "اروپا",
+	//			type: "line",
+	//			borderColor: "#673ab7",
+	//			data: [408, 547, 675, 734],
+	//			fill: false
+	//		}, {
+	//			label: "آفریقا",
+	//			type: "line",
+	//			borderColor: "#f02769",
+	//			data: [133, 221, 783, 2478],
+	//			fill: false
+	//		}, {
+	//			label: "اروپا",
+	//			type: "bar",
+	//			backgroundColor: "rgba(0,0,0,0.2)",
+	//			data: [408, 547, 675, 734],
+	//		}, {
+	//			label: "آفریقا",
+	//			type: "bar",
+	//			backgroundColor: "rgba(0,0,0,0.2)",
+	//			backgroundColorHover: "#3e95cd",
+	//			data: [133, 221, 783, 2478]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			display: false
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'رشد جمعیت (میلیون) : اروپا و آفریقا'
+	//		}
+	//	}
+	//});
+	//// chart 10
+	//new Chart(document.getElementById("chart10"), {
+	//	type: 'bubble',
+	//	data: {
+	//		labels: "اروپا",
+	//		datasets: [{
+	//			label: ["چین"],
+	//			backgroundColor: "#17a00e",
+	//			borderColor: "#17a00e",
+	//			data: [{
+	//				x: 21269017,
+	//				y: 5.245,
+	//				r: 15
+	//			}]
+	//		}, {
+	//			label: ["دانمارک"],
+	//			backgroundColor: "#ffc107",
+	//			borderColor: "#ffc107",
+	//			data: [{
+	//				x: 258702,
+	//				y: 7.526,
+	//				r: 10
+	//			}]
+	//		}, {
+	//			label: ["آلمان"],
+	//			backgroundColor: "#17a00e",
+	//			borderColor: "#17a00e",
+	//			data: [{
+	//				x: 3979083,
+	//				y: 6.994,
+	//				r: 15
+	//			}]
+	//		}, {
+	//			label: ["ژاپن"],
+	//			backgroundColor: "#29cc39",
+	//			borderColor: "#29cc39",
+	//			data: [{
+	//				x: 4931877,
+	//				y: 5.921,
+	//				r: 15
+	//			}]
+	//		}]
+	//	},
+	//	options: {
+	//		maintainAspectRatio: false,
+	//		legend: {
+	//			rtl: true,
+	//		},
+	//		tooltips: {
+	//			rtl: true
+	//		},
+	//		title: {
+	//			display: true,
+	//			text: 'جمعیت پیش بینی شده جهان (میلیون) در 2050'
+	//		},
+	//		scales: {
+	//			yAxes: [{
+	//				scaleLabel: {
+	//					display: true,
+	//					labelString: "خوشبختی"
+	//				}
+	//			}],
+	//			xAxes: [{
+	//				scaleLabel: {
+	//					display: true,
+	//					labelString: "تولید ناخالص داخلی"
+	//				}
+	//			}]
+	//		}
+	//	}
+	//});
+});
